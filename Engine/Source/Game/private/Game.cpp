b7cd608da3a9da8ee7ad1124dcf0ac73502efbf5
iHR4K <sergeynewcastle@yahoo.com>
2025-02-12 23:22:48 -0500

    missed call in the game class

--- Engine/Source/Game/private/Game.cpp
+++ Engine/Source/Game/private/Game.cpp
@@ -50,6 +50,8 @@ namespace cqe
 			// The most common idea for such a loop is that it returns false when quit is required, or true otherwise
 			quit = !PlatformLoop();
 		}
+
+		m_renderThread->WaitForRenderEngineToShutdown();
 	}
 
 	void Game::Update(float dt)
